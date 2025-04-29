dd94526b8eb72d2573b384604d5a7d25ec644283
renanA11 <vetka3985@gmail.com>
2025-04-29 19:32:32 -0300

    contadorjava

---
+++ src/Contador.java
@@ -0,0 +1,26 @@
+https://github.com/renanA11/Contador-java.git
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("1");
+		int parametroUm = terminal.??;
+		System.out.println("2");
+		int parametroDois = terminal.??;
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (? exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws  ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+	}
+}
